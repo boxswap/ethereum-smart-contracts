e3cc1249680aa0c2c98c61356261092ed76b3b14
Charles Marino <charlesmarino1@gmail.com>
2019-03-11 18:46:27 +0700

    update

--- ERC721ProductKey.sol
+++ ERC721ProductKey.sol
@@ -1076,24 +1076,11 @@ contract IERC721ProductKey is IERC721 {
     function name() external view returns (string memory);
     function symbol() external view returns (string memory);
     function tokenURI(uint256 tokenId) external view returns (string memory);
-    function supportsInterface(bytes4 interfaceId) external view returns (bool);
-    function balanceOf(address owner) public view returns (uint256 balance);
-    function ownerOf(uint256 tokenId) public view returns (address owner);
-    function approve(address to, uint256 tokenId) public;
-    function getApproved(uint256 tokenId) public view returns (address operator);
-    function setApprovalForAll(address operator, bool _approved) public;
-    function isApprovedForAll(address owner, address operator) public view returns (bool);
-    function transferFrom(address from, address to, uint256 tokenId) public;
-    function safeTransferFrom(address from, address to, uint256 tokenId) public;
-    function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory data) public;
     function activate(uint256 _tokenId) external;
     function purchase(uint256 _productId, address _beneficiary) external returns (uint256);
     function setKeyAttributes(uint256 _keyId, address _attributes) public;
     function keyInfo(uint256 _keyId) public;
     function isKeyActive(uint256 _keyId) public view returns (bool);
-    function productInfo(uint256 _productId) public view returns (uint256, uint256, uint256, uint256, uint256);
-    function getAllProductIds() public view returns (uint256[] memory);
-    function priceOf(uint256 _productId) public view returns (uint256);
 
     event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
     event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId);
@@ -1118,7 +1105,7 @@ contract IERC721ProductKey is IERC721 {
     );
 }
 
-contract ERC721ProductKey is ERC721, ERC721Enumerable, ReentrancyGuard, IERC721Metadata, ProductInventory, IERC721ProductKey {
+contract ERC721ProductKey is ERC721, ERC721Enumerable, ReentrancyGuard, IERC721Metadata, ProductInventory {
     using SafeMath for uint256;
 
     // Token name
@@ -1296,11 +1283,10 @@ contract ERC721ProductKey is ERC721, ERC721Enumerable, ReentrancyGuard, IERC721M
 
     function setKeyAttributes(
         uint256 _keyId,
-        address _attributes
+        uint256 _attributes
     )
     external
     onlyMinter
-    returns (uint256)
     {
         return _setKeyAttributes(
             _keyId,
